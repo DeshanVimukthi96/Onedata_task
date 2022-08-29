6fcc04fe6d8ca8de8bc518e7a12dd64806a69a2b
Deshan Vimukthi <74645681+DeshanVimukthi96@users.noreply.github.com>
2022-08-29 23:11:34 +0530

    Add files via upload

---
+++ add_num_app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+
+
+# In[3]:
+
+
+a = st.number_input('Enter a number1')
+b = st.number_input('Enter a number2')
+
+
+# In[4]:
+
+
+st.write('The total number is ', a+b)
+
+
+# In[ ]:
+
+
+
+
